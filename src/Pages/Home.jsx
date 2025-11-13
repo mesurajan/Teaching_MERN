90b7bdb760f73a7ad73ec22f30f6df5851fcf1d9
mesurajan <surajansth1995@gmail.com>
2025-11-13 23:36:54 +0545

    updated

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+     <div className='text-green-500'>Home i m surajan shrestha</div>
+    </>
+   
+  )
+}
+
+export default Home
